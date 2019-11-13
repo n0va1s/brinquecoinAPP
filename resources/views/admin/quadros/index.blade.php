95de2bf8cf7089d9d5b073726df72727430559bd
João Paulo Novais <joaopaulonovais@gmail.com>
2019-11-13 06:44:13 -0300

    Design da tela meus quadros

--- resources/views/admin/quadros/index.blade.php
+++ resources/views/admin/quadros/index.blade.php
@@ -4,18 +4,53 @@
 <div class="container">
     <h3 class="center">Meus quadros</h3>
     <div class="row">
-        <div class="col s4 m7">
+        <div class="col s4 m4">
             <div class="card">
                 <div class="card-image">
-                    <img src="images/sample-1.jpg">
-                    <span class="card-title">Card Title</span>
+                    <img src="{{asset('img/tiposquadros/viagem.jpg')}}">
                 </div>
                 <div class="card-content">
-                    <p>I am a very simple card. I am good at containing small bits of information.
-                    I am convenient because I require little markup to use effectively.</p>
+                    <p>Quadro de Viagem</p>
+                    <br />
+                    <p>João Pedro</p>
                 </div>
-                <div class="card-action">
-                    <a href="#">This is a link</a>
+                <div>
+                    <div class="card-action center">
+                         <a class="waves-effect waves-light btn-small green darken-2">Duplicar</a>
+                         <a class="waves-effect waves-light btn-small red darken-2">Encerrar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col s4 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="{{asset('img/tiposquadros/mesada.jpg')}}">
+                </div>
+                <div class="card-content">
+                    <p>Quadro de Mesada</p>
+                    <br />
+                    <p>Eduardo</p>
+                </div>
+                <div class="card-action center">
+                    <a class="waves-effect waves-light btn-small green darken-2">Duplicar</a>
+                    <a class="waves-effect waves-light btn-small red darken-2">Encerrar</a>
+                </div>
+            </div>
+        </div>
+        <div class="col s4 m4">
+            <div class="card">
+                <div class="card-image">
+                    <img src="{{asset('img/tiposquadros/tarefas.jpg')}}">
+                </div>
+                <div class="card-content">
+                    <p>Quadro de Tarefas</p>
+                    <br />
+                    <p>Helena</p>
+                </div>
+                <div class="card-action center">
+                    <a class="waves-effect waves-light btn-small green darken-2">Duplicar</a>
+                    <a class="waves-effect waves-light btn-small red darken-2">Encerrar</a>
                 </div>
             </div>
         </div>
