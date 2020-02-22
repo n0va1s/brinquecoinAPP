351718715daf521a31cfb1b412c4df6850651993
João Paulo Novais <joaopaulonovais@gmail.com>
2020-02-22 20:54:11 -0300

    :bug: sending email when open capsules fixed

--- resources/views/mail/openCapsule.blade.php
+++ resources/views/mail/openCapsule.blade.php
@@ -2,7 +2,7 @@
 <img width="100em" height="100em" src="{{ $imLogo }}">
 <br />
 Olá {{ $nmRecipient }},<br />
-Em {{ $dtCreatedAt }}, {{ $nmSender }} criou essa cápsula do tempo para vc abrir hoje. <br />
+Em {{ date('d/m/Y', strtotime($dtCreatedAt)) }}, {{ $nmSender }} criou essa cápsula do tempo para vc abrir hoje. <br />
 
 Esta foi a menssagem:<br />
 {{ $txMessage }}<br />
