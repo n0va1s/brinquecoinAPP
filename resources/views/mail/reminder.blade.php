a590a134170186d58f39e0c238c1ab779f8acbd2
João Paulo Novais <joaopaulonovais@gmail.com>
2020-08-24 10:44:07 -0300

    :lipstick: message to cancel board reminder

--- resources/views/mail/reminder.blade.php
+++ resources/views/mail/reminder.blade.php
@@ -7,6 +7,10 @@ Não deixe pra cuidar dos seus filhos e de vc depois.
 @component('mail::button', ['url' => $link])
 Abrir quadro agora
 @endcomponent
+<br />
 Um abraço,<br />
 <b>Time {{ config('app.name') }}</b>
+Caso vc não queira mais receber este lembrete, por favor
+acesse app.brinquecoin.com e na lista dos seus quadros 
+selecione a opção "Fechar Quadro"
 @endcomponent
